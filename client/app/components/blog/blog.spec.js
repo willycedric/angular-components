c8548e1ffb144d75f89c2c68c3f70f0300a3494e
Willy DONGMO <w.dongmo@gmail.com>
2016-03-23 23:16:45 +0100

    add some explanations throught comments

--- client/app/components/blog/blog.spec.js
+++ client/app/components/blog/blog.spec.js
@@ -6,8 +6,12 @@ import {BlogController} from './blog.controller';
 describe('Blog', () => {
   let $rootScope;
   let makeController;
-
+  //load modules that we want to test
+  //This function registers a module configuration code. 
+  //It collects the configuration information which will be used when the injector is created by inject.
+  //docs --> https://docs.angularjs.org/api/ngMock/function/angular.mock.module
   beforeEach(window.module(blog.name));
+  // inject() is used to inject arguments of all given functions (see https://docs.angularjs.org/api/ngMock/function/angular.mock.inject)
   beforeEach(inject(_$rootScope_ => {
     $rootScope = _$rootScope_;
     makeController = (injectables) => {
@@ -50,6 +54,8 @@ describe('Blog', () => {
       expect(controller.posts).to.be.an('array');
       expect(controller.posts[0]).to.have.property('author');
       expect(controller.posts[0]).to.have.property('title');
+      expect(controller.posts.length).to.equal(9);
+      expect(controller.posts[0].author).to.equal('Casidy James');
     });
   });
 
--- spec.bundle.js
+++ spec.bundle.js
@@ -1,7 +1,7 @@
 /*
  * When testing with webpack and ES6, we have to do some extra
  * things get testing to work right. Because we are gonna write test
- * in ES6 to, we have to compile those as well. That's handled in
+ * in ES6 too, we have to compile those as well. That's handled in
  * karma.conf.js with the karma-webpack plugin. This is the entry
  * file for webpack test. Just like webpack will create a bundle.js
  * file for our client, when we run test, it well compile and bundle them
